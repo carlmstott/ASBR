0b58354ee4e44fec36682f10f3404ced71accaad
Bharat Mathur <bharat.mathur@hotmail.com>
2023-03-03 17:18:18 -0600

    Create defineRobot.m

---
+++ THA2/defineRobot.m
@@ -0,0 +1,31 @@
+function [robot, err] = defineRobot(M, S, B)
+r.M = M;
+r.S = S;
+r.B = B;
+
+if(size(S, 2) ~= size(B, 2))
+    disp('Error: Screw axes base frame and end-effector frame size mismatch')
+    robot = 0;
+    err = '1';
+    return
+else
+    r.numJoints = size(S, 2);
+end
+
+% Calculate SE(3) form for base screw axes, S
+S_se_3 = eye(4);
+for i=1:r.numJoints
+    S_se_3(:, :, i) = [skew(S(i).w) S(i).v;0 0 0 0];
+end
+r.S_se_3 = S_se_3; 
+
+% Calculate SE(3) form for end effector screw axes, B
+B_se_3 = eye(4);
+for i=1:r.numJoints
+    B_se_3(:, :, i) = [skew(S(i).w) S(i).v;0 0 0 0];
+end
+r.B_se_3 = B_se_3; 
+
+robot = r;
+
+end
