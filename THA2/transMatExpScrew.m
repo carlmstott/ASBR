a1533ee348a684c7bbe040bda52e824370c6256d
Bharat Mathur <bharat.mathur@hotmail.com>
2023-03-14 23:59:17 -0400

    added transMatExpScrew

--- THA2/transMatExpScrew.m
+++ THA2/transMatExpScrew.m
@@ -12,5 +12,11 @@ function T=transMatExpScrew(S,theta)
 R = matExpRodrigues(S.w,theta);
 
 % Week 5 - Lecture 2 slide 11
+if(R ~= eye(3))
+% if joint is revolute
 T = [R, (eye(3) * theta + S.w * (1 - cos(theta)) + S.w * S.w * (theta - sin(theta))) * S.v; 0, 0, 0, 1];
+else
+    %if joint is prismatic
+    T = [R, S.v * theta; 0, 0, 0, 1];
+end
 end
