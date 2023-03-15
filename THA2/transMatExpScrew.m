ed991b8713909c8576ebafae12901bd7589d120d
Bharat Mathur <bharat.mathur@hotmail.com>
2023-03-14 23:33:46 -0400

    added transMatExpScrew

---
+++ THA2/transMatExpScrew.m
@@ -0,0 +1,16 @@
+% brief: converts screw axis, S (w, v) to 4x4 homogenous transformation
+% matrix to represent travel along screw axis by theta
+% params:
+% S: Screw axis (w, v), 6x1 vector
+% theta: angle of rotation
+%
+% returns:
+% T: 4x4 homogenous transformation matrix to represent travel along screw axis
+
+function T=transMatExpScrew(S,theta)
+
+R = matExpRodrigues(S.w,theta);
+
+% Week 5 - Lecture 2 slide 11
+T = [R, (eye(3) * theta + S.w * (1 - cos(theta)) + S.w * S.w * (theta - sin(theta))) * S.v; 0, 0, 0, 1];
+end
