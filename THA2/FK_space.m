ebb645729ee05ff38024a3a1b0b10b877459b5ec
Bharat Mathur <bharat.mathur@hotmail.com>
2023-03-16 21:25:58 -0400

    added FK_space.m

---
+++ THA2/FK_space.m
@@ -0,0 +1,30 @@
+%brief: calculates forward kinematics in space frame
+% params:
+% robot: robot object consistig of the robot's kinematic properties
+% defined in defineRobot.m 
+% jointAngles: list of joint angles 0 to n
+% returns:
+% T: forward kinematics transformation matrix in space frame 4x4
+
+function [T, err] = FK_space(robot,jointAngles)
+
+if length(jointAngles) ~= robot.numJoints
+    error("Error: make sure vector of angles has same length as robot has joints")
+    err = -1; %#ok<UNRCH>
+end
+
+MatrixExponentals=eye(4);
+
+% calculate screw matrix exponentials (4x4) and multiply them from joint 1
+% to n. e_S1_theta1 * e_S2_theta2 ... * e_Sn_thetan . Ref: W6-L2 slide 5
+
+
+for i = 1 : robot.numJoints
+    MatrixExponentals = MatrixExponentals * transMatExpScrew(robot.S, jointAngles(i));
+end
+
+% product of exponentials (e_S1_theta1 * e_S2_theta2 ... * e_Sn_thetan) * M . Ref: W6-L2 slide 5
+T = MatrixExponentals * robot.M;
+
+err = 0;
+end
