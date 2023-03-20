f84c4599bcdfc7a6cc5b0d4360bc0f33ddf5c802
Bharat Mathur <bharat.mathur@hotmail.com>
2023-03-20 01:19:34 -0400

    fix transMatExpSscre to take twist as matrixC

--- THA2/transMatExpScrew.m
+++ THA2/transMatExpScrew.m
@@ -1,22 +1,22 @@
 % brief: converts screw axis, S (w, v) to 4x4 homogenous transformation
 % matrix to represent travel along screw axis by theta
 % params:
-% S: Screw axis (w, v), 6x1 vector
+% twist: twist vector (w, v), 6x1
 % theta: angle of rotation
 %
 % returns:
-% T: 4x4 homogenous transformation matrix to represent travel along screw axis
+% T: 4x4 homogenous transformation matrix to represent travel by theta along screw axis
 
-function T=transMatExpScrew(S,theta)
+function T=transMatExpScrew(twist,theta)
 
-R = matExpRodrigues(S.w,theta);
+R = matExpRodrigues(twist(1:3),theta);
 
 % Week 5 - Lecture 2 slide 11
 if(R ~= eye(3))
 % if joint is revolute
-T = [R, (eye(3) * theta + S.w * (1 - cos(theta)) + S.w * S.w * (theta - sin(theta))) * S.v; 0, 0, 0, 1];
+T = [R, (eye(3) * theta + twist(1:3) * (1 - cos(theta)) + twist(1:3) * twist(1:3) * (theta - sin(theta))) * twist(4:6); 0, 0, 0, 1];
 else
     %if joint is prismatic
-    T = [R, S.v * theta; 0, 0, 0, 1];
+    T = [R, twist(4:6) * theta; 0, 0, 0, 1];
 end
 end
