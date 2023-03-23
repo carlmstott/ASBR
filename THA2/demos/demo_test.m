96072fbe256e9d60a2079cf894651a1578870166
Bharat Mathur <bharat.mathur@hotmail.com>
2023-03-23 01:40:25 -0400

    added new demos folder

---
+++ THA2/demos/demo_test.m
@@ -0,0 +1,32 @@
+clear; clc;
+
+M = [1 0 0 3;0 1 0 0;0 0 1 0;0 0 0 1];
+
+S(:, 1) = [0; 0; 1; 0; 0; 0];
+
+S(:, 2) = [0; 0; 0; 1; 0; 0];
+
+S(:, 3) = [0; 0; 1; 0; -1; 0];
+
+S(:, 4) = [0; -1; 0; -1; 0; -1];
+
+S(:, 5) = [0; 0.707; 0.707; 0.707; -1.414; 1.414];
+
+B(:, 1) = [0; 0; 1; 0; 3; 0];
+
+B(:, 2) = [0; 0; 0; 1; 0; 0];
+
+B(:, 3) = [0; 0; 1; 0; 2; 0];
+
+B(:, 4) = [0; -1; 0; -1; 0; 2];
+
+B(:, 5) = [0; 0.707; 0.707; 0.707; 0.707; -0.707];
+
+%we need to define a theta vector here
+[robot, err] = defineRobot(M, S, B);
+
+jointAngles = sym('theta', [robot.numJoints 1]);
+
+[T, err] = FK_space(robot,jointAngles)
+
+J = J_space(robot,jointAngles)
