a8d40999ebffd66e73f6e9a7a3b7492bb7f5a96c
carlmstott <cstott@utexas.edu>
2023-03-27 21:28:16 -0500

    double checked FK_body_test

--- THA2/test/FK_body_test.m
+++ THA2/test/FK_body_test.m
@@ -2,21 +2,22 @@
 %provided on the modern robotics github page:
 %(https://github.com/NxRLab/ModernRobotics/blob/master/packages/MATLAB/mr/FKinBody.m
 
-clear all;%close all;
+clear all;
+close all;
 
 
 M = [[-1, 0, 0, 0]; [0, 1, 0, 6]; [0, 0, -1, 2]; [0, 0, 0, 1]];
 
 Blist = [[0; 0; -1; 2; 0; 0], [0; 0; 0; 0; 1; 0], [0; 0; 1; 0; 0; 0.1]];
 
-jointangles = [pi / 2; 3; pi];
+screwangles = [pi / 2; 3; pi];
 
 S=Blist; %the S entry in define robot is used to track the fwd kinumatics,which we are not using
 
 robot=defineRobot(M, S, Blist);
 
 
-[BodyK, jointTransformations]=FK_body(robot,jointangles);
+[BodyK, jointTransformations]=FK_body(robot,screwangles);
 
 
 
