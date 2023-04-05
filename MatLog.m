01060c23f3f8bc817a0380e3daa7435eeb3c961a
carlmstott <cstott@utexas.edu>
2023-04-05 14:41:13 -0500

    made sure MatLog function is right

--- MatLog.m
+++ MatLog.m
@@ -5,17 +5,17 @@ Tmatrix=se3(TMatrix);
 if rotm(Tmatrix)==eye(3)
     w=[0;0;0];
     v=trvec(Tmatrix);
-    v=transpose(v)
+    v=transpose(v);
 else
- [theta,omega]=axis_angle_code(rotm(Tmatrix));
+ [theta,skewW]=axis_angle_code(rotm(Tmatrix));
 
-w=omega;
 
-GinvTheta=(1/theta)*eye(3)-(1/2)*w+((1/theta)-(1/2)*(cot(theta/2)))*w*w;
+w=[-skewW(2,3);skewW(1,3);-skewW(1,2)]; %converting from skewsym back to a vector
+
+GinvTheta=((1/theta)*eye(3)) - ((1/2)*skew(w)) + ( (1/theta) - (1/2)*(cot(theta/2)) )*skew(w)*skew(w);
 
 v=GinvTheta*transpose(trvec(Tmatrix));
 
-w=[-w(2,3);w(1,3);-w(1,2)]; %converting from skewsym back to a vector
 
 end
 Twist=[w;v];
