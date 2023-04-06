ccb481299c1f71714cab2d40d1bfd2f7b4d27524
Bharat Mathur <bharat.mathur@hotmail.com>
2023-04-06 14:18:48 -0400

    changed BM's IK to damped least squares for now

--- THA2/workspace/IK.m
+++ THA2/workspace/IK.m
@@ -6,7 +6,13 @@ twist_error_EE_frame = MatLog(TransInv(T_base_ee) * desiredPoseTransMat);
 
 while i < maxIter
 i = i + 1;
-currJointAngles = double(currJointAngles + pinv(J_body(robot, currJointAngles)) * twist_error_EE_frame);
+% currJointAngles = double(currJointAngles + pinv(J_body(robot, currJointAngles)) * twist_error_EE_frame);
+
+%%
+J_J_transpose = J_body(robot, currJointAngles) * transpose(J_body(robot, currJointAngles));
+currJointAngles = double(currJointAngles + transpose(J_body(robot, currJointAngles)) *inv((J_J_transpose + 0.5 * 0.5*eye(size(J_J_transpose)))) * twist_error_EE_frame);
+
+%%
 T_base_ee = double(FK_space(robot, currJointAngles, false));
 twist_error_EE_frame = MatLog(TransInv(T_base_ee) * desiredPoseTransMat);
 
