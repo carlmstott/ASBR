f6fbdcd51d1c87f235e5fe2c88dd5e87a84d2120
Bharat Mathur <bharat.mathur@hotmail.com>
2023-04-20 21:05:14 -0400

    initial push least squares registration. to be tested

---
+++ THA3/least_squares_registration.m
@@ -0,0 +1,33 @@
+% A and B are column vectors i.e. A = [x1 y1 z1; 
+                                     % x2 y2 z2;
+                                     % .
+                                     % .
+                                     %         ]
+function T = least_squares_registration(A, B)
+
+% calculate centroids
+A_centroid = mean(A);
+B_centroid = mean(B);
+
+% calculate vectors from centroids
+A_prime = A - A_centroid; % A_tilda = ai - mean_A
+B_prime = B - B_centroid; % B_tilda = bi - mean_B
+
+H = zeros(3,3);
+
+for i = 1: size(A, 1)
+    H = H + A_prime(i, :) * transpose(B_prime(i, :));
+end
+
+[U, S, V] = svd(H);
+
+R = V * transpose(U);
+
+% check R here ....
+
+p = B_centroid - R * A_centroid;
+
+T = [R p; 0 0 0 1] ;
+
+
+end
