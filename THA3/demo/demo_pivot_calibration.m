0bac89e47a8c2ab2b1d6326d1283b957588f1d2e
Bharat Mathur <bharat.mathur@hotmail.com>
2023-04-23 16:53:28 -0500

    added pivot calibration demo

--- THA3/demo/demo_pivot_calibration.m
+++ THA3/demo/demo_pivot_calibration.m
@@ -1,7 +1,19 @@
 clc; clear;
-lines = readlines('C:\Users\bhara\Documents\GitHub\ASBR\THA3\data\HW3-PA1\pa1-debug-a-empivot.txt');
+lines = readlines('C:\Users\bhara\Documents\GitHub\ASBR\THA3\data\HW3-PA1\pa1-debug-b-empivot.txt');
 readTxt
-lines = readlines('C:\Users\bhara\Documents\GitHub\ASBR\THA3\data\HW3-PA1\pa1-debug-a-optpivot.txt');
+lines = readlines('C:\Users\bhara\Documents\GitHub\ASBR\THA3\data\HW3-PA1\pa1-debug-b-optpivot.txt');
 readTxt
 format long g
 
+% use the first frame to define a local probe coordinate system
+% calculate centroid
+G_centroid = mean(G_i(:, :, 1));
+
+% compute g_i
+for i = 1:N_frames
+g_i(:, :, i) = G_i(:, :, i) - G_centroid;
+end
+
+[T, err] = least_squares_registration(g_i(:, :, 7), G_i(:,:,7));
+
+pivotCalibration(T)
