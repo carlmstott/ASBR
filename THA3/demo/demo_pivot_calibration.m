4ffbf528de8ddb3ad94e17b6b1ba43031e2a5a8f
Bharat Mathur <bharat.mathur@hotmail.com>
2023-04-23 16:39:14 -0500

    initialize demo for pivot calibration

---
+++ THA3/demo/demo_pivot_calibration.m
@@ -0,0 +1,7 @@
+clc; clear;
+lines = readlines('C:\Users\bhara\Documents\GitHub\ASBR\THA3\data\HW3-PA1\pa1-debug-a-empivot.txt');
+readTxt
+lines = readlines('C:\Users\bhara\Documents\GitHub\ASBR\THA3\data\HW3-PA1\pa1-debug-a-optpivot.txt');
+readTxt
+format long g
+
