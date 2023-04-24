7a9913a295a905b49915f33aabad5758e30d7691
Bharat Mathur <bharat.mathur@hotmail.com>
2023-04-24 15:41:53 -0500

    fix pivot calibration to use tall matrix

--- THA3/pivotCalibration.m
+++ THA3/pivotCalibration.m
@@ -1,12 +1,25 @@
 function [b_tip, b_post] = pivotCalibration (T_k)
-R_k = T_k(1:3, 1:3);
-p_k = T_k(1:3, 4);
 
-A = [R_k, -eye(3,3)];
-B = -p_k;
+A = [];
+B = [];
+for i = 1:size(T_k, 3)
+    
+    % extract rotation and translation
+    R_i = T_k(1:3, 1:3, i);
+    p_i = T_k(1:3, 4, i);
 
+    % for A and B matrices
+    A_i = [R_i, -eye(3, 3)];
+    B_i = -p_i;
+    A = [A; A_i]
+    B = [B; B_i]
+
+end
+
+% solve AX = B
 X = mldivide(A, B);
 
+% extract probe parameters
 b_tip = X(1:3);
 b_post = X(4:6);
 end
