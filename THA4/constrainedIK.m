fc8625f17462fee57b7a364f547637312a11607d
Bharat Mathur <bharat.mathur@hotmail.com>
2023-04-25 23:52:42 -0500

    apply joint limits

---
+++ THA4/constrainedIK.m
@@ -0,0 +1,93 @@
+% brief: calculates inverse kinematics using Newton Raphson method while
+% handling singularity configurations using Damped Least Squares method
+% robot: robot object consistig of the robot's kinematic properties
+% defined in defineRobot.m
+% currJointAngles: current robot configuration
+% desiredPoseTransMat: desired EE position as a translation matrix
+% maxIter: maximum number of iterations
+% threshDist: stopping criteria for translations
+% threshOr: stopping criteria for orientations
+% plot: boolean value to plot (or not) the robot's frames at each iteration
+% returns:
+% currJointAngles: joint configuration to achieve desired pose
+% err: error code
+
+
+function [currJointAngles, allJacobians, allNormOrient, allNormTrans, err] = constrainedIK(robot, currJointAngles, desiredPoseTransMat, maxIter, threshDist, threshOr, plot)
+desiredPoseTransMat = double(desiredPoseTransMat);
+i = 0;
+T_base_ee = Fk_Space_for_Kuka(robot,currJointAngles, plot);
+
+gif('kukaAnimationDLS.gif','Delaytime',1/4,'loopcount',15)
+
+twist_error_EE_frame = MatLog(TransInv(T_base_ee) * desiredPoseTransMat);
+
+%calculate stopping criterion
+distanceError = norm(twist_error_EE_frame(4:6));
+orientationError = norm(twist_error_EE_frame(1:3));
+
+jointLimits_min = deg2rad([-185;
+    -140;
+    -120;
+    -350;
+    -125;
+    -350;]);
+
+jointLimits_max = deg2rad([185;
+    -5;
+    -168;
+    350;
+    125;
+    350;] / 2);
+
+jointLimits_median = (jointLimits_min + jointLimits_max) / 2;
+
+
+while ((i < maxIter) && (distanceError > threshDist) && (orientationError > threshOr))
+    i = i + 1;
+    allJacobians(:,:,i) = J_body(robot, currJointAngles);
+    allNormTrans(i) = distanceError;
+    allNormOrient(i) = orientationError;
+
+    hold OFF;
+    
+    % Calculate Jacobian and Moore-Penrose Pseudoinverse
+    J = J_body(robot, currJointAngles);
+    J_dagger = pinv(J);
+
+    % Calculate H function for joint limits and its gradient
+    H = 0;
+    jointAngles = sym('theta', [robot.numJoints 1]);
+
+    for H_iter = 1 : robot.numJoints
+        H = H + ((jointAngles(H_iter) - jointLimits_median(H_iter)) / (jointLimits_median(H_iter) - jointLimits_max(H_iter))) ^ 2;
+    end
+    H = H / robot.numJoints;
+    grad_H = gradient(H);
+
+    grad_H = double(subs(grad_H, jointAngles, currJointAngles))
+
+    delta_theta = 0.1 * J_dagger * twist_error_EE_frame + (eye(robot.numJoints) - J_dagger * J) * 10* grad_H;
+    currJointAngles = double(currJointAngles + delta_theta);
+
+    rad2deg(currJointAngles)
+
+    % update
+    T_base_ee = double(Fk_Space_for_Kuka(robot, currJointAngles, plot));
+    gif
+    twist_error_EE_frame = MatLog(TransInv(T_base_ee) * desiredPoseTransMat);
+
+    %calculate stopping criterion
+    distanceError = norm(twist_error_EE_frame(4:6));
+    orientationError = norm(twist_error_EE_frame(1:3));
+    pause(0.1)
+
+
+    if(i == maxIter)
+        disp(" did not converge")
+        err = -1;
+    else
+        err = 0;
+    end
+
+end
---
+++ THA4/constrained_IK_test.m
@@ -0,0 +1,101 @@
+% This script tests the DLS_inverse_kinematics using the Kuka KR120 robot.
+% The initial configuration input to the IK solver is a singulaarity. The
+% Solver detects this by calculating the isotropy (> inf at singularity)
+% and switches to using the Damped Least Squares algorithm. Once out of
+% singularity, it switches to using Newton-Raphson method to get the IK
+% solution"
+
+clear all;
+close all; clc;
+
+% Screw Axes for a Kuka KR120
+M(:,:,1)=[1,0,0,0
+      0,1,0,0
+      0,0,1,0
+      0,0,0,1];
+
+M(:,:,2)=[1,0,0,0
+          0,1,0,3.30
+          0,0,1,4.65
+          0,0,0,1];
+
+M(:,:,3)=[1,0,0,0
+          0,1,0,3.30
+          0,0,1,17.95
+          0,0,0,1];
+
+M(:,:,4)=[1,0,0,0
+          0,1,0,9.40
+          0,0,1,19.10
+          0,0,0,1];
+
+M(:,:,5)=[1,0,0,0
+          0,1,0,15.50
+          0,0,1,19.10
+          0,0,0,1];
+
+
+M(:,:,6)=[1,0,0,0
+          0,1,0,17.65
+          0,0,1,19.10
+          0,0,0,1];
+
+
+S=sym('S', [6,6]);
+
+S(:,1)=[0; 0; 1; 0; 0; 0];
+
+S(:,2)=[-1; 0; 0; 0; -6.45; 3.30];
+
+S(:,3)=[-1; 0; 0; 0; -19.10; 3.30];
+
+S(:,4)=[0; 1; 0; -19.10; 0; 0];
+
+S(:,5)=[-1; 0; 0; 0; -19.10; 15.50];
+
+S(:,6)=[0; 1; 0; -19.10; 0; 0];
+
+
+
+B=sym('B', [6,6]);
+
+B(:,1)=[0; 0; 1; 0; -17.65; 0];
+
+B(:,2)=[-1; 0; 0; 0; 12.65; -14.35];
+
+B(:,3)=[-1; 0; 0; 0; 0; -14.35];
+
+B(:,4)=[0; 1; 0; 0; 0; 0];
+
+B(:,5)=[-1; 0; 0; 0; 0; -2.15];
+
+B(:,6)=[0; 1; 0; 0; 0; 0];
+
+B=double(B);
+
+S=double(S);
+ 
+ robot = defineRobot(M, S, B);
+
+jointAngles = randi([-314, 314], robot.numJoints, 1) / 100; %joint angles used to generate a reachable T which
+                            %will be the goal of our FK function
+
+%below is an example target end effector pose that was randomly generated
+% T=[0.3161   -0.9272    0.2011   -7.8102
+%     0.8429    0.3717    0.3890    7.1492
+%     -0.4354    0.0465    0.8990   16.0442
+%     0         0         0    1.0000]
+
+
+[T, err] = FK_space(robot,jointAngles, 0); %this gives us the T we want to 
+                                           %reach with our FK function
+
+[currJointAngles, allJacobians, allNormOrient, allNormTrans, err] = constrainedIK(robot, randi([-174, 174], robot.numJoints, 1) / 100, T, 500,0.01, 0.01, false);
+
+[TafterIK, err] = FK_space(robot,currJointAngles, 0);
+
+T
+
+TafterIK
+
+ellipsoidPlotter(allJacobians, allNormOrient, allNormTrans)
