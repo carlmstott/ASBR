e41dce9fe6d38987dc8d5227dbb07a35b95e7ef4
Bharat Mathur <bharat.mathur@hotmail.com>
2023-04-27 15:26:05 -0500

    updates

--- THA3/solutions/problem_3_b.m
+++ THA3/solutions/problem_3_b.m
@@ -6,6 +6,9 @@ readTxt
 format long g
 for i = 1 : N_frames
     F_A(:, :, i) = least_squares_registration(a_i, A_i(:,:,i));
+    figure; hold on
+    plotter(a_i, A_i(:,:,i), F_A(:, :, i))
+
 end
 
 F_A
---
+++ THA3/solutions/problem_4.m
@@ -0,0 +1,27 @@
+clc; clear;
+lines = readlines('C:\Users\bhara\Documents\GitHub\ASBR\THA3\data\HW3-PA1\pa1-debug-b-empivot.txt');
+readTxt
+lines = readlines('C:\Users\bhara\Documents\GitHub\ASBR\THA3\data\HW3-PA1\pa1-debug-b-optpivot.txt');
+readTxt
+format long g
+
+% use the first frame to define a local probe coordinate system
+% calculate centroid
+G_centroid = mean(G_i(:, :, 1));
+figure; hold on;
+% compute g_i
+for i = 1:N_frames
+    g_i(:, :, i) = G_i(:, :, i) - G_centroid;
+
+    [T, err] = least_squares_registration(g_i(:, :, i), G_i(:,:,i));
+
+    plotter(g_i(:, :, i), G_i(:,:,i), T)
+
+    T_tall(:, :, i) = T;
+    pause(2)
+
+    clf('reset')
+
+end
+
+[b_tip, b_post] = pivotCalibration(T_tall)
